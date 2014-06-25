32d3dec7beaf6e56a0427314a2bf635bf811796a
Drew Haines <drewhaines@yahoo.com>
2014-06-25 13:20:00 -0700

    Sharing is caring

---
+++ Large Adder.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : LargeAdders.c
+ Author      : Yusuf Ozturk
+ Version     :
+ Copyright   : Copyleft - no rights reserved. Please modify and distribute as you see necessary
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	char number1[256];
+	char number2[256];
+	char result[256];
+
+	addLarge(number1,number2,result);
+return EXIT_SUCCESS;
+}
